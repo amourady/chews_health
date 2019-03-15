4f07b473fce4b5e89e23ee51cd9d9e5e6cf93865
gjhbfdty2 <32973523+gjhbfdty2@users.noreply.github.com>
2019-03-14 21:40:59 -0700

    downgrade location package version, it now works with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,269 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:location/location.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ChewsHealth',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      // appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(50.0),
+      //   child: AppBar(
+      //     title: Text('ChewsHealth'),
+      //   ),
+      // ),
+      body: ListView(
+
+        children: [
+          Row(
+            mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                heroTag: "b1",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondRoute()),
+                  );
+                },         
+                tooltip: 'Go to user page',
+                  child: Icon(Icons.person),
+                ),
+
+              FloatingActionButton(
+                heroTag: "b2",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyHomePage()),
+                  );
+                },         
+                tooltip: 'Go to location page',
+                  child: Icon(Icons.location_on),
+                ),
+
+              FloatingActionButton(
+                heroTag: "b3",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ThirdRoute()),
+                  );
+                },         
+                tooltip: 'Go to run page',
+                  child: Icon(Icons.directions_run),
+                ),
+
+            ],
+          ),
+          // Image.asset("images/Header.png",
+          // fit: BoxFit.cover,
+          // ),
+          ///////////////////
+          RaisedButton(
+          child: Text('Go to route2'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+          ),
+          ////////////////////////////
+          RaisedButton(
+          child: Text('Go to route3'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ThirdRoute()),
+            );
+          },
+        ),
+         RaisedButton(
+          child: Text('GPS Test route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+          },
+        ),
+        FloatingActionButton(
+          heroTag: "b4",
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ThirdRoute()),
+            );
+          },         
+          tooltip: 'get my location',
+            child: Icon(Icons.gps_fixed),
+          ),
+
+
+
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
 
-  final String title;
+
+class ThirdRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+
 class _MyHomePageState extends State<MyHomePage> {
+  String _counter = "";
+  var location = new Location();
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+
+      // location.onLocationChanged().listen((LocationData currentLocation) {
+      //   print(currentLocation.latitude);
+      //   print(currentLocation.longitude);
+      //   print(currentLocation.accuracy);
+      //   print(currentLocation.altitude);
+      //   print(currentLocation.speed);
+
+    location.onLocationChanged().listen((Map<String,double> currentLocation) {
+      print(currentLocation["latitude"]);
+      print(currentLocation["longitude"]);
+      print(currentLocation["accuracy"]);
+      print(currentLocation["altitude"]);
+      print(currentLocation["speed"]);
+      print(currentLocation["speed_accuracy"]); // Will always be 0 on iOS
+    });
+
+  
+      
+      //   _counter = "lat: " + currentLocation.latitude.toString() +
+      //  "\nlong: " + currentLocation.longitude.toString() +
+      //  "\nspeed: " + currentLocation.speed.toString() +
+      //  "\n\nUsing Geolocator:";
+      // });
+ 
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+
+        //title: Text(widget.title),
+
+        /*
+        "Yo Im a noob and I got this error because I had some pages with an optional title param.
+         It wasn't giving me warning or failure on compile because I was giving the Text a
+          variable argument that hadn't been set yet. Then I never set it in the constructor for the page */
+        title: Text("GPS Test"),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              onPressed: null,
-              child: const Text('Locate Me'),
+            Text(
+              "Here's the lat, long, and speed: ",
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Geolocate',
+        child: Icon(Icons.gps_fixed),
       ),
     );
   }
